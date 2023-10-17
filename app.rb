bb468731c0ba81933f89172f5973fb700b3ead72
Mariana Cardozo <146393028+maricardozo-booth@users.noreply.github.com>
2023-10-17 19:09:25 +0000

    working structure

--- app.rb
+++ app.rb
@@ -2,8 +2,51 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  erb(:rules)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+
+  @opo_move = moves.sample
+  if @opo_move == "rock"
+    @outcome = "lost"
+  elsif @opo_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+
+  erb(:scissors)
+end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  @opo_move = moves.sample
+  if @opo_move == "rock"
+    @outcome = "tied"
+  elsif @opo_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:rock)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  @opo_move = moves.sample
+  if @opo_move == "rock"
+    @outcome = "won"
+  elsif @opo_move == "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
+
+  erb(:paper)
 end
